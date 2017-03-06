9f87e834f77008123427f421702ca0873e1a4f9f
Ruiping.Yin <zzrabs@gmail.com>
2017-03-06 11:59:39 +1100

    New Code of convEncoder

---
+++ convEncoder.py
@@ -0,0 +1,166 @@
+"""Tutorial on how to create a convolutional autoencoder w/ Tensorflow.
+
+Parag K. Mital, Jan 2016
+"""
+import tensorflow as tf
+import numpy as np
+import math
+
+def lrelu(x, leak=0.2, name="lrelu"):
+    with tf.variable_scope(name):
+        f1 = 0.5 * (1 + leak)
+        f2 = 0.5 * (1 - leak)
+        return f1 * x + f2 * abs(x)
+
+def autoencoder(input_shape=[None, 784],
+                n_filters=[1, 10, 10, 10],
+                filter_sizes=[3, 3, 3, 3]):
+    """
+
+    Parameters
+    ----------
+    input_shape : list, optional
+        Description
+    n_filters : list, optional
+        0: channel number
+        1: 0 layer conv kernel number
+    filter_sizes : list, optional
+        Description
+
+    Returns
+    -------
+    x : Tensor
+        Input placeholder to the network
+    z : Tensor
+        Inner-most latent representation
+    y : Tensor
+        Output reconstruction of the input
+    cost : Tensor
+        Overall cost to use for training
+        
+    """
+    
+    # input to the network
+    x = tf.placeholder(tf.float32, input_shape, name='x')
+    
+    # ensure 2-d is converted to square tensor.
+    if len(x.get_shape()) == 2:
+        x_dim = np.sqrt(x.get_shape().as_list()[1])
+        if x_dim != int(x_dim):
+            raise ValueError('Unsupported input dimensions')
+        x_dim = int(x_dim)
+        x_tensor = tf.reshape(x, [-1, x_dim, x_dim, n_filters[0]])
+    elif len(x.get_shape()) == 4:
+        x_tensor = x
+    else:
+        raise ValueError('Unsupported input dimensions')
+    current_input = x_tensor
+    
+    # Build the encoder
+    encoder = []
+    shapes = []
+    for layer_i, n_output in enumerate(n_filters[1:]):
+        n_input = current_input.get_shape().as_list()[3]        # RGB or Gray
+        shapes.append(current_input.get_shape().as_list())
+        W = tf.Variable(
+            tf.random_uniform([
+                filter_sizes[layer_i],
+                filter_sizes[layer_i],
+                n_input, n_output],
+                -1.0 / math.sqrt(n_input),
+                1.0 / math.sqrt(n_input)))
+        b = tf.Variable(tf.zeros([n_output]))
+        encoder.append(W)
+        output = lrelu(
+            tf.add(tf.nn.conv2d(
+                current_input, W, strides=[1, 4, 4, 1], padding='SAME'), b))
+        current_input = output
+
+    # %%
+    # store the latent representation
+    z = current_input
+    encoder.reverse()
+    shapes.reverse()
+
+    # %%
+    # Build the decoder using the same weights
+    for layer_i, shape in enumerate(shapes):
+        W = encoder[layer_i]
+        b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
+        output = lrelu(tf.add(
+            tf.nn.conv2d_transpose(
+                current_input, W,
+                tf.pack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                strides=[1, 4, 4, 1], padding='SAME'), b))
+        current_input = output
+
+    # %%
+    # now have the reconstruction through the network
+    y = current_input
+    # cost function measures pixel-wise difference
+    cost = tf.reduce_sum(tf.square(y - x_tensor))
+
+    # %%
+    return {'x': x, 'z': z, 'y': y, 'cost': cost}
+
+
+# %%
+def ConvAutocoder():
+    import tensorflow as tf
+    import matplotlib  
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    from matplotlib.pyplot import savefig
+    
+    # load data
+    import scipy.io as scio
+    data = scio.loadmat('/scratch/data2mat2.mat')
+    data = data['dataset']
+    mean_img = np.mean(data, axis=0)
+    
+    # create autoencoder
+    ae = autoencoder([None, 445, 402, 3], [3, 10], [16, 16])
+    learning_rate = 0.3
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(ae['cost'])
+
+    # %%
+    # We create a session to use the graph
+    sess = tf.Session()
+    sess.run(tf.initialize_all_variables())
+    
+    # save model
+    saver = tf.train.Saver()
+    saver.restore(sess, './convEncoder299.tfmodel')
+    # %%
+    # Fit all training data
+    batch_size = 100
+    n_epochs = 100
+    for epoch_i in range(n_epochs):
+        for batch_i in range(data.shape[0] // batch_size):
+            batch_xs = data[batch_i * batch_size:batch_i * batch_size + batch_size, ...]
+            train = np.array([img - mean_img for img in batch_xs])
+            sess.run(optimizer, feed_dict={ae['x']: train})
+        print(epoch_i, sess.run(ae['cost'], feed_dict={ae['x']: train}))
+        saver.save(sess, './convEncoder' + str(epoch_i) + '.tfmodel')
+
+    # %%
+    # Plot example reconstructions
+    n_examples = 10
+    test_xs = data[100:110, ...]
+    test_xs_norm = np.array([img - mean_img for img in test_xs])
+    recon = sess.run(ae['y'], feed_dict={ae['x']: test_xs_norm})
+    print('Recon Shape: ', recon.shape)
+    fig, axs = plt.subplots(2, n_examples, figsize=(10, 2))
+    for example_i in range(n_examples):
+        axs[0][example_i].imshow(
+            np.reshape(test_xs[example_i, :], (445, 402, 3)))
+        axs[1][example_i].imshow(
+            np.reshape(
+                np.reshape(recon[example_i, ...], (445, 402, 3)) + mean_img,
+                (445, 402, 3)))
+    savefig('./result2.jpg')
+
+
+# %%
+if __name__ == '__main__':
+    ConvAutocoder()
